4fe6f953248d83de6f8782ee3a1b0f09eac9beff
沉沦 <354769733@qq.com>
2022-12-31 23:00:53 +0800

    修改了R619AC的feeds

--- R619AC/R619AC.sh
+++ R619AC/R619AC.sh
@@ -10,6 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 # 修改主机名字，把PandoraBox修改你喜欢的就行（不能纯数字或者使用中文）
-#sed -i '/uci commit system/i\uci set system.@system[0].hostname='PandoraBox'' package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='PandoraBox'' package/lean/default-settings/files/zzz-default-settings
 # 版本号里显示一个自己的名字（ababwnq build $(TZ=UTC-8 date "+%Y.%m.%d") @ 这些都是后增加的）
-#sed -i 's/OpenWrt /编译时间 $(TZ=UTC-8 date "+%Y.%m.%d") @ 沉沦 /g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/OpenWrt /编译时间 $(TZ=UTC-8 date "+%Y.%m.%d") @ 沉沦 /g' package/lean/default-settings/files/zzz-default-settings
