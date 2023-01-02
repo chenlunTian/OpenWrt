3e9916495c5eb9b70f98b31eee058036bfe5cd1c
沉沦 <354769733@qq.com>
2023-01-02 22:02:03 +0800

    2

--- MI-R4A/diy-part1.sh
+++ MI-R4A/diy-part1.sh
@@ -11,8 +11,8 @@
 #
 
 # 添加插件源码
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
 
 ### 修改为R4A千兆版Breed直刷版
 ## mt7621_xiaomi_mir3g-v2.dts 好像被改成了 mt7621_xiaomi_mi-router-4a-3g-v2.dtsi  测试一下
