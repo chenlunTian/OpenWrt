5699e95a29a7a2802d16ad6051d56437e122d922
chenlunTian <354769733@qq.com>
2023-04-19 03:18:09 +0800

    X86-64添加iStore

--- X86_64/diy-part1.sh
+++ X86_64/diy-part1.sh
@@ -14,5 +14,5 @@ sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.con
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-
+sed -i '$a src-git istore https://github.com/linkease/istore;main' feeds.conf.default
 
