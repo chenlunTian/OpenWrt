b443bc8683480790824a904ddc39f744745e5664
chenlunTian <354769733@qq.com>
2024-07-31 22:42:52 +0800

    切换编译内核为5.15

---
+++ X86_64/diy-part1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+
+#切换成5.15内核
+git checkout 8cf859bf012c9619d56cf90e567a381edd1119d8
+# 修改Makefile
+sed -i '/KERNEL_PATCHVER/d' ./target/linux/x86/Makefile
+sed -i '/^KERNEL_TESTING_PATCHVER/i KERNEL_PATCHVER:=5.15' ./target/linux/x86/Makefile
+
+echo "run scripts update && install"
+
+./scripts/feeds update -a
+./scripts/feeds install -a
