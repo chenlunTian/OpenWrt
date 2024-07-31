cf8672dcbdba579c83bde107a84d6089b04e4d37
沉沦 <354769733@qq.com>
2024-07-31 13:59:24 +0800

    Update X86_64.sh
    1

--- X86_64/X86_64.sh
+++ X86_64/X86_64.sh
@@ -17,17 +17,52 @@ sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/fil
 sed -i '11s/ucidef_set_interface_lan/ucidef_set_interface_wan/g' package/base-files/files/etc/board.d/99-default_network
 sed -i '12s/ucidef_set_interface_wan/ucidef_set_interface_lan/g' package/base-files/files/etc/board.d/99-default_network
 
-# # 修改版本为编译日期+自己名字
-# date_version=$(date +"%y.%m.%d")
-# orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-# sed -i "s/${orig_version}/R${date_version} by tzq/g" package/lean/default-settings/files/zzz-default-settings
+echo "remove packages"
+# 替换的包 
+
+# luci-app-serverchan
+rm -rf feeds/luci/applications/luci-app-serverchan
+git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
+
+# mosdns
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+git clone https://github.com/sbwml/luci-app-mosdns.git
+mv luci-app-mosdns/luci-app-mosdns ./package/luci-app-mosdns
+mv luci-app-mosdns/mosdns ./package/mosdns
+rm -rf luci-app-mosdns
+
+# luci-app-adguardhome
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+
+# luci-app-netdata
+rm -rf feeds/luci/applications/luci-app-netdata
+git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
+
+git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
+# msd_lite
+rm -rf feeds/packages/net/msd_lite
+git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
+git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
+
+# luci-app-eqosplus
+git clone https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
+
+# ddns-go
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+
+# 添加netspeedtest
+git clone --depth=1 https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
+
+# 添加advancedplus
+git clone https://github.com/sirpdboy/luci-app-advancedplus.git package/luci-app-advancedplus
 
 
-# # 修改 Makefile
-# find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
-# find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
-# find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHREPO/PKG_SOURCE_URL:=https:\/\/github.com/g' {}
-# find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
+# openclash
+git clone https://github.com/chenlunTian/luci-app-openclash.git package/luci-app-openclash
 
 # 设置root密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
