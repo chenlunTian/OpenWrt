8966c930909cfa8e599b1b6e50e7fcb779684be5
沉沦 <354769733@qq.com>
2024-07-31 09:53:29 +0800

    Update X86_64.sh
    修改

--- X86_64/X86_64.sh
+++ X86_64/X86_64.sh
@@ -17,115 +17,17 @@ sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/fil
 sed -i '11s/ucidef_set_interface_lan/ucidef_set_interface_wan/g' package/base-files/files/etc/board.d/99-default_network
 sed -i '12s/ucidef_set_interface_wan/ucidef_set_interface_lan/g' package/base-files/files/etc/board.d/99-default_network
 
-# 修改版本为编译日期+自己名字
-date_version=$(date +"%y.%m.%d")
-orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-sed -i "s/${orig_version}/R${date_version} by tzq/g" package/lean/default-settings/files/zzz-default-settings
-
-# TTYD 自动登录
-# sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
-echo "remove packages"
-# 移除要替换的包
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-netgear
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/luci/applications/luci-app-serverchan
-echo "add packages"
-
-git clone https://github.com/Lienol/openwrt-package.git
-mv openwrt-package/luci-app-filebrowser ./package/luci-app-filebrowser
-mv openwrt-package/luci-app-ssr-mudb-server ./package/luci-app-ssr-mudb-server
-
-git clone -b openwrt-18.06 https://github.com/immortalwrt/luci.git luci1
-mv luci1/applications/luci-app-eqos ./package/luci-app-eqos
-
-git clone https://github.com/xiaorouji/openwrt-passwall.git
-mv openwrt-passwall/luci-app-passwall ./package/luci-app-passwall
-
-git clone https://github.com/xiaorouji/openwrt-passwall2.git
-mv openwrt-passwall2/luci-app-passwall2 ./package/luci-app-passwall2
-
-mkdir openclash
-cd openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull --depth 1 origin master
-git branch --set-upstream-to=origin/master master
-mv luci-app-openclash ./package/luci-app-openclash
-cd ..
-
-git clone https://github.com/haiibo/packages.git packages2
-mv packages2/luci-theme-atmaterial ./package/luci-theme-atmaterial
-mv packages2/luci-theme-netgear ./package/luci-theme-netgear
-mv packages2/luci-theme-opentomcat ./package/luci-theme-opentomcat
-mv packages2/luci-app-onliner ./package/luci-app-onliner
-
-git clone https://github.com/sbwml/luci-app-mosdns.git
-mv luci-app-mosdns/luci-app-mosdns ./package/luci-app-mosdns
-mv luci-app-mosdns/mosdns ./package/mosdns
-
-git clone https://github.com/linkease/nas-packages-luci.git
-mv nas-packages-luci/luci/luci-app-ddnsto ./package/luci-app-ddnsto
-
-git clone https://github.com/linkease/nas-packages.git
-mv nas-packages/network/services/ddnsto package/ddnsto
-
-rm -rf nas-packages nas-packages-luci packages2 openclash openwrt-passwall2 openwrt-passwall openwrt-package luci1 luci-app-mosdns
-
-# 添加额外插件
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
-git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
+# # 修改版本为编译日期+自己名字
+# date_version=$(date +"%y.%m.%d")
+# orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+# sed -i "s/${orig_version}/R${date_version} by tzq/g" package/lean/default-settings/files/zzz-default-settings
 
 
-# 科学上网插件
-git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-svn export https://github.com/haiibo/packages/trunk/luci-app-vssr package/luci-app-vssr
-git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb package/lua-maxminddb
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-
-
-# Themes
-git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
-git clone --depth=1 https://github.com/chenlunTian/luci-theme-argon_armygreen package/luci-theme-argon_armygreen
-
-# SmartDNS
-git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
-
-# msd_lite
-git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
-git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
-
-sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
-sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
-chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
-
-# 添加netspeedtest
-git clone --depth=1 https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
-
-#添加lucky
-git clone --depth=1 https://github.com/gdy666/luci-app-lucky.git package/lucky
-# 修复 hostapd 报错
-cp -f $GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
-
-# 修改 Makefile
-find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
-find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
-find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHREPO/PKG_SOURCE_URL:=https:\/\/github.com/g' {}
-find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
+# # 修改 Makefile
+# find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
+# find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
+# find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHREPO/PKG_SOURCE_URL:=https:\/\/github.com/g' {}
+# find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
 
 # 设置root密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
