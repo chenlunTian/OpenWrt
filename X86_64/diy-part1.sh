ca2253a43bfbb3db93467c7bcbd245ae0015abda
沉沦 <354769733@qq.com>
2024-09-09 22:25:29 +0800

    Update diy-part1.sh
    6.1

--- X86_64/diy-part1.sh
+++ X86_64/diy-part1.sh
@@ -2,10 +2,10 @@
 #
 
 #切换成5.15内核
-git checkout -f 8cf859bf012c9619d56cf90e567a381edd1119d8
+#git checkout -f 8cf859bf012c9619d56cf90e567a381edd1119d8
 # 修改Makefile
-sed -i '/KERNEL_PATCHVER/d' ./target/linux/x86/Makefile
-sed -i '/^KERNEL_TESTING_PATCHVER/i KERNEL_PATCHVER:=5.15' ./target/linux/x86/Makefile
+#sed -i '/KERNEL_PATCHVER/d' ./target/linux/x86/Makefile
+#sed -i '/^KERNEL_TESTING_PATCHVER/i KERNEL_PATCHVER:=5.15' ./target/linux/x86/Makefile
 
 echo "run scripts update && install"
 
