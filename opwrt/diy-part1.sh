ef2d77f97cadb078e6477df44ff71418d3823285
chenlunTian <354769733@qq.com>
2024-09-18 02:32:17 +0800

    修复

--- opwrt/diy-part1.sh
+++ opwrt/diy-part1.sh
@@ -2,7 +2,7 @@
 #
 #自行编译时，会出现内核的魔法值不一样，需要完成如下修改：
 #获取值
-curl -s https://downloads.openwrt.org/releases/23.05.4/targets/x86/64/openwrt-23.05.4-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
+curl -s https://downloads.openwrt.org/releases/23.05.0/targets/x86/64/openwrt-23.05.0-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
 #修改内核配置文件
 sed -i '121s/^/#/' include/kernel-defaults.mk
 sed -i '121a\    cp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' include/kernel-defaults.mk
