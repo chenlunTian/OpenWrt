446d82c8678ca465b008bcd304ed65baaa793e7a
chenlunTian <354769733@qq.com>
2024-09-18 21:32:48 +0800

    删除vermagic设置

--- opwrt/diy-part1.sh
+++ opwrt/diy-part1.sh
@@ -2,10 +2,10 @@
 #
 #自行编译时，会出现内核的魔法值不一样，需要完成如下修改：
 #获取值
-curl -s https://downloads.openwrt.org/releases/23.05.2/targets/x86/64/openwrt-23.05.2-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
-#修改内核配置文件
-sed -i '121s/^/#/' include/kernel-defaults.mk
-sed -i '121a\    cp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' include/kernel-defaults.mk
+# curl -s https://downloads.openwrt.org/releases/23.05.2/targets/x86/64/openwrt-23.05.2-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
+# #修改内核配置文件
+# sed -i '121s/^/#/' include/kernel-defaults.mk
+# sed -i '121a\    cp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' include/kernel-defaults.mk
 
 sed -i '1,4s/^/#/' feeds.conf.default
 
