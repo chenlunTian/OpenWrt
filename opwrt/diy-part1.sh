e44f082d98012fe076301c135e5235c6d5d4cff2
chenlunTian <354769733@qq.com>
2024-09-18 21:28:28 +0800

    修复

--- opwrt/diy-part1.sh
+++ opwrt/diy-part1.sh
@@ -2,10 +2,21 @@
 #
 #自行编译时，会出现内核的魔法值不一样，需要完成如下修改：
 #获取值
-curl -s https://downloads.openwrt.org/releases/23.05.0/targets/x86/64/openwrt-23.05.0-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
+curl -s https://downloads.openwrt.org/releases/23.05.2/targets/x86/64/openwrt-23.05.2-x86-64.manifest | grep kernel | awk '{print $3}' | awk -F- '{print $3}' > vermagic
 #修改内核配置文件
 sed -i '121s/^/#/' include/kernel-defaults.mk
 sed -i '121a\    cp $(TOPDIR)/vermagic $(LINUX_DIR)/.vermagic' include/kernel-defaults.mk
+
+sed -i '1,4s/^/#/' feeds.conf.default
+
+sed -i '1 i src-git routing https://git.openwrt.org/feed/routing.git;openwrt-23.05' feeds.conf.default
+sed -i '1 i src-git telephony https://git.openwrt.org/feed/telephony.git;openwrt-23.05' feeds.conf.default
+sed -i '1 i src-git packages https://github.com/immortalwrt/packages.git;openwrt-23.05' feeds.conf.default
+sed -i '1 i src-git luci https://github.com/immortalwrt/luci.git;openwrt-23.05' feeds.conf.default
+
+
+
+
 echo "run scripts update && install"
 
 
