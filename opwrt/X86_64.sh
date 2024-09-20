22dcea09de9f87ec087b6fd6b3cae3ebd8cdf6a3
沉沦 <354769733@qq.com>
2024-09-20 15:14:00 +0800

    Update X86_64.sh

--- opwrt/X86_64.sh
+++ opwrt/X86_64.sh
@@ -49,10 +49,10 @@ git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adg
 #wwan-packages
 git clone https://github.com/immortalwrt/wwan-packages.git package/awwan-packages
 
-
+rm -rf package/kernel/quectel-qmi-wwan
+rm -rf package/kernel/fibocom-qmi-wwan
 
 echo "run scripts update && install"
 
 ./scripts/feeds update -a
-rm -rf package/feeds/packages/fibocom-qmi-wwan
 ./scripts/feeds install -a
